1216eb76a024a42179e63e62178c353134997e6a
eduuwem <eduuwem.udoh@gmail.com>
2023-10-31 06:41:14 -0700

    task1

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""print the alphabet in lowercase, not followed by a newmline."""
+"""print the alphabet in lowercase, not followed by a new line."""
 
 for letter in range(97, 123):
-    print("{}.format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
