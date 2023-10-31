5e1997f30fc37a970f1d6bffb277bf394d897b8d
eduuwem <eduuwem.udoh@gmail.com>
2023-10-31 13:00:27 -0700

    task13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,7 +8,7 @@
  * Return: If the function fails - NULL.
  * 	Otherwise - a pointer to the new node.
  */
-listint_t insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head, *new;
 
