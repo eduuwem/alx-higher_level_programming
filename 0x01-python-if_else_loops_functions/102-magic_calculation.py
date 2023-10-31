c784cc8715d73269b2a51e936e0a939a37063abf
eduuwem <eduuwem.udoh@gmail.com>
2023-10-31 13:35:11 -0700

    task16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # Author -  Bamidele Adefolaju 
 
-def remove_char_at(str , n):
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+def magic_calculation(a, b, c):
+    """Match bytecode povided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
 
