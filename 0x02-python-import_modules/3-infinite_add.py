40e67a7de94825f61b2963ffa5fce64c36f04232
eduuwem <eduuwem.udoh@gmail.com>
2023-11-02 07:05:50 -0700

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     import sys, math
 
     result = 0
-
     for i in sys.argv:
-
         result += int(i)
-
-        print("{}".format(result))
+    print("{}".format(result))
