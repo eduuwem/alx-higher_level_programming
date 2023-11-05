b2bd1237c0dad45512f450807ad471815c5dda6b
eduuwem <eduuwem.udoh@gmail.com>
2023-11-05 07:59:56 -0800

    task11

--- 0x02-python-import_modules/11-delete_at.py
+++ 0x02-python-import_modules/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if 0 <= idx < len(my_list):
-        modified_list = []
-        for k in range(len(my_list)):
-            if k != idx:
-                modified_list.append(my_list[k])
+        modified_list = my_list[:idx] + my_list[idx + 1:]
         my_list = modified_list
     return my_list
