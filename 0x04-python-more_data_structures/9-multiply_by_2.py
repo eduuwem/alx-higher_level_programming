01ade2450394f05e92d311e6156537f74123dbdc
eduuwem <eduuwem.udoh@gmail.com>
2023-11-08 12:05:06 -0800

    task9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    my_dictionary = dictionary(a_dictionary)
-    for b, v in dictionary.items():
-        my_dictionary[k] = v * 2
-    return my_dictionary
+def multiply_by_2(my_dict):
+    return {key: val*2 for key, val in my_dict.items()}
