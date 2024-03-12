091089822a3fdace384f590f2c787adac67ad409
eduuwem <eduuwem.udoh@gmail.com>
2024-03-12 10:15:28 -0700

    task10

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+# Author - Tolulope Fakunle
+"""print the numbers from 1 to 100 seperated by a space.
+    For multiples of three, print Fizz instead of the number
+    For multiples of five, print Buzz instead of the number.
+    For multiples of three and five, print FizzBuzz instead of the number.
+    """
+
+
 def fizzbuzz():
-    for i in range(1, 101):
-	if i % 3 == 0:
-	   print('Fizz', end='')
-	if i % 5 == 0:
-	   print('Buzz', end='')
-	if i % 3 != 0 and i % 5 != 0:
-	   print(i, end='')
-	print(' ', end='')
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,54 +1,34 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include "lists.h"
 
 /**
- * insert_node - inserts a node in proper place of sorted list
- *
- * @head: first node of linked list
- * @number: number to add in linked list
- * Return: Address of new node or NULL if failure
+ * insert_node - inserts a number into a sorted linked list
+ * @head: A pointer to the head of linked list
+ * @number: The number to insert
+ * Author - Tolulope Fakunle
+ * Return: If the function fails - NULL.
+ * 	Otherwise - a pointer to the new node.
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *n_node;
-	listint_t *tmp;
+	listint_t *node = *head, *new;
 
-	if (!head)
-		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+        	return (NULL);
+	new->n = number;
 
-	n_node = malloc(sizeof(listint_t));
-	if (!n_node)
-		return (NULL);
-	n_node->n = number;
-
-	tmp = *head;
-	if (tmp)
+	if (node == NULL || node-> n >= number)
 	{
-		if (tmp->n >= number)
-		{
-			n_node->next = tmp;
-			*head = n_node;
-		}
-		else
-		{
-			while (tmp->next)
-			{
-				if (tmp->next->n < number)
-					tmp = tmp->next;
-				else
-					break;
-			}
-			n_node->next = tmp->next;
-			tmp->next = n_node;
-		}
+		new->next = node;
+		*head = new;
+		return (new);
 	}
-	else
-	{
-		*head = n_node;
-		n_node->next = NULL;
-	}
-	return (n_node);
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+	
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
