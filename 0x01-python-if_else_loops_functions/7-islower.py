0ca41877c5c8b7992ab797a2b05ec6fa17b10ec8
eduuwem <eduuwem.udoh@gmail.com>
2024-03-12 09:58:00 -0700

    task6

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+# Author - Tolulope Fakunle
+
 def islower(c):
-    if ord(c) < 123 and ord(c) > 96:
-	return (True)
-    return (False)
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
