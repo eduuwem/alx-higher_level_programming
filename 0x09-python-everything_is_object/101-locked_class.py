fff2b6b614b34e04f176bea0e0204926053fcd69
eduuwem <eduuwem.udoh@gmail.com>
2024-04-09 07:39:26 -0700

    everything

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attribute"""
+    __slots__ = ["first_name"]
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,2 @@
+Read me
+Every thing is object
